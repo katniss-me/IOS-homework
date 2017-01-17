d919d4f2e84f9551961cd00c6dd7e584a41114f6
i.minjung.chun <indie.me.chun@gmail.com>
2017-01-17 20:23:39 +0900

    Test add Person

---
+++ TestProject/TestProject/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestProject
+//
+//  Created by katniss on 2017. 1. 17..
+//  Copyright © 2017년 katniss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestProject/TestProject/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestProject
+//
+//  Created by katniss on 2017. 1. 17..
+//  Copyright © 2017년 katniss. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
